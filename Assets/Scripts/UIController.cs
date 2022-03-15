58219f31bccc4e52ec97ed452f011fcd2a6d0e14
IgorGalimski <igorg@fm-games.com>
2022-03-15 21:36:20 +0200

    Fix saving screenshot

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.IO;
+using UnityEditor;
 using UnityEngine;
 
 namespace Xternity
@@ -7,9 +8,7 @@ namespace Xternity
     public class UIController : MonoBehaviour
     {
         public Camera Camera;
-
-        public int Wight = 1080;
-        public int Height = 1920;
+        public RenderTexture RenderTexture;
         
         public void TakeScreenshot()
         {
@@ -19,20 +18,28 @@ namespace Xternity
         public IEnumerator TakeScreenshotIenumerator()
         {
             yield return new WaitForEndOfFrame();
+
+            Camera.targetTexture = RenderTexture;
             
             RenderTexture activeRenderTexture = RenderTexture.active;
             RenderTexture.active = Camera.targetTexture;
  
             Camera.Render();
+
+            var targetTexture = Camera.targetTexture;
             
-            Texture2D image = new Texture2D(Camera.targetTexture.width, Camera.targetTexture.height);
-            image.ReadPixels(new Rect(0, 0, Camera.targetTexture.width, Camera.targetTexture.height), 0, 0);
+            var image = new Texture2D(targetTexture.width, targetTexture.height);
+            image.ReadPixels(new Rect(0, 0, targetTexture.width, targetTexture.height), 0, 0);
             image.Apply();
 
             var bytes = image.EncodeToPNG();
             File.WriteAllBytes(Path.Combine(Application.dataPath, "Images", "test.png"), bytes);
             
             RenderTexture.active = activeRenderTexture;
+
+            Camera.targetTexture = null;
+            
+            AssetDatabase.Refresh();
             
             Debug.Log("Save screenshot");
         }
